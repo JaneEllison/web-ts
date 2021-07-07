028c17010168763870db4013a1301d985c74ea42
Anastasiya Sekerich <asekerich99@gmail.com>
2021-07-07 23:32:17 +0300

    refactor: reimplement functionality using composition style

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'ffff', age: 0 });
+const user = new User({ id: 1, name: 'Pasta', age: 9 });
 
-user.events.on('change', () => {
-  console.log('change');
+user.on('save', () => {
+  console.log(user);
 });
 
-user.events.trigger('change');
+user.save();
