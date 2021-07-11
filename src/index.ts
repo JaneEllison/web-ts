ac73f2721fc218cdc17a5b8a0230d28726fd690f
Anastasiya Sekerich <asekerich99@gmail.com>
2021-07-11 14:41:52 +0300

    feat: add UserEdit andUserShow classes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
-import { UserForm } from './views/UserForm';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserEdit } from './views/UserEdit';
+import { User, UserProps } from './models/User';
 
 const user = User.buildUser({ name: 'Alya', age: 33 });
 
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+
+  userEdit.render();
+
+  console.log(userEdit);
 } else {
   throw new Error('Root element not found');
 }
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
